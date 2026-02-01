1fda5f613656296f5ff8870a37c001d9d3341295
Snehal377 <snehalpawar0096@gmail.com>
2026-02-01 23:47:38 +0530

    Add files via upload

---
+++ preprocess.py
@@ -0,0 +1,23 @@
+import re
+
+def clean_text(text):
+    text = re.sub(r"http\S+", "", text)
+    text = re.sub(r"@\w+", "", text)
+    text = re.sub(r"#\w+", "", text)
+    text = text.lower()
+    text = re.sub(r"[^a-zA-Z\s]", "", text)
+    return text
+
+
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+
+analyzer = SentimentIntensityAnalyzer()
+
+def get_sentiment(text):
+    score = analyzer.polarity_scores(text)["compound"]
+    if score >= 0.05:
+        return "Positive"
+    elif score <= -0.05:
+        return "Negative"
+    else:
+        return "Neutral"
